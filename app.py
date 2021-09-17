958d3cd4bc4e989f13276d5ab65407e4ea41878e
Siddharth Sharma <ssiddharth408@gmail.com>
2021-09-18 00:02:58 +0530

    column name as dictionary added to mushroom_data.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def main():
     return render_template('index.html',
                            cap_shape=md.cap_shape,
+                           cap_surface=md.cap_surface,
+                           cap_color=md.cap_color,
+                           bruises=md.bruises,
+                           odor=md.odor,
+                           gill_attachment=md.gill_attachment,
+                           gill_spacing=md.gill_spacing,
+                           gill_size=md.gill_size,
+                           gill_color=md.gill_color,
                            )
 
 
